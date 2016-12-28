dfa5073f0815d2b7a127cf7eedb0b7099952d577
Adam Soltys <asoltys@gmail.com>
2016-12-28 14:34:39 -0800

    Prioritization filler

--- src/components/prioritization.vue
+++ src/components/prioritization.vue
@@ -5,6 +5,11 @@
         .page-header
           h1 POPs Toolkit
       .col-md-12.bodytext
-        p
-  div hello
+        p One of the primary goals of the POPs toolkit is to provide countries with the capacity to perform simple Human Health Risk Assessments. When it comes to cleaning up multiple sites in a country the questions arise: where to begin?  Which sites should be a priority for a risk assessment? 
+        p In this section, two semi-quantitative tools are presented that allow the user to determine which sites should be assessed and then to prioritize sites based on their potential for causing unacceptable risks to humans and/or the natural environment.
+        p The purpose of the site prioritization is to classify contaminated sites based on their need for further action. Further action usually means risk assessment.  The tools provided ask the user a series of questions regarding: contaminant characteristics, off-site migration potential, exposure and socio-economic factors. Then, based on the answers provided, calculates a total score for that site.
+
+        .well
+          | POPs Toolkit users who are looking to prioritize and manage their pesticide stockpiles should view more information about the 
+          a(href="http://popstoolkit.com/fao+psms.aspx") FAO's obsolete pesticide programme.
 </template>
