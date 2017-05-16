6d77f38f28d6fdffd8e07968a72359224133c1e9
Adam Soltys <asoltys@gmail.com>
2017-05-16 09:21:21 -0700

    CDIderm and CDIwater calcs

--- src/components/hhra.vue
+++ src/components/hhra.vue
@@ -20,12 +20,33 @@
       hqparam(name='EF', units='days/year', v-model='EF')
       hqparam(name='ED', units='years', v-model='ED')
       hqparam(name='ET', units='mg/d', v-model='ET')
-      hqparam(name='VFs', units='unitless', v-model='VFs')
-      hqparam(name='PEFw', units='unitless', v-model='PEFw')
+      hqparam(name='VFs', units='m3/kg', v-model='VFs')
+      hqparam(name='PEFw', units='m3/kg', v-model='PEFw')
       hqparam(name='AT', units='days', v-model='AT')
 
-    input(v-model='banana')
-    input(v-model='banana')
+    h2 Dermal contact with contaminated soil Dose Calculation
+    div `CDIderm = (Csoil*EF*ED*SA*AF*ABSd*0.000001)/(AT*BW) = `
+    input.result(v-model='CDIderm', ref='CDIderm')
+    table
+      hqparam(name='Csoil', units='mg/kg', v-model='Csoil')
+      hqparam(name='EF', units='days/year', v-model='EF')
+      hqparam(name='ED', units='years', v-model='ED')
+      hqparam(name='SA', units='cm2/day', v-model='SA')
+      hqparam(name='AF', units='mg/cm2', v-model='AF')
+      hqparam(name='ABSd', units='proportion', v-model='ABSd')
+      hqparam(name='AT', units='days', v-model='AT')
+      hqparam(name='BW', units='kg', v-model='BW')
+
+    h2 Water Ingestion Dose Calculation
+    div `CDIwater = (Cwater*0.001*EF*ED*IRw)/(AT*BW) = `
+    input.result(v-model='CDIwater', ref='CDIwater')
+    table
+      hqparam(name='Csoil', units='mg/kg', v-model='Csoil')
+      hqparam(name='EF', units='days/year', v-model='EF')
+      hqparam(name='ED', units='years', v-model='ED')
+      hqparam(name='IRw', units='cm2/day', v-model='IRw')
+      hqparam(name='AT', units='days', v-model='AT')
+      hqparam(name='BW', units='kg', v-model='BW')
 </template>
 
 <script>
@@ -41,12 +62,16 @@ export default {
       EF: '',
       ED: '',
       IRs: '',
+      IRw: '',
       RBA: '',
       AT: '',
       BW: '',
       VFs: '',
       PEFw: '',
-      ET: ''
+      ET: '',
+      ABSd: '',
+      SA: '',
+      AF: ''
     }
   },
   methods: {
@@ -73,6 +98,22 @@ export default {
     },
     CDIinhal: function () {
       let v = (this.Csoil * this.EF * this.ED * this.ET * ((1 / (this.VFs)) + (1 / (this.PEFw))) / (this.AT))
+      if (!isNaN(v) && isFinite(v)) {
+        return v
+      } else {
+        return ''
+      }
+    },
+    CDIderm: function () {
+      let v = (this.Csoil * this.EF * this.ED * this.SA * this.AF * this.ABSd * 0.00001) / (this.AT * this.BW)
+      if (!isNaN(v) && isFinite(v)) {
+        return v
+      } else {
+        return ''
+      }
+    },
+    CDIwater: function () {
+      let v = (this.Csoil * this.EF * this.ED * this.IRw) / (this.AT * this.BW)
       if (!isNaN(v) && isFinite(v)) {
         return v
       } else {
