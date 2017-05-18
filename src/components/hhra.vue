18d226d261255586770b7c1518d35bbd9e9b5315
Adam Soltys <asoltys@gmail.com>
2017-05-18 12:13:50 -0700

    dynamic data table presets

--- src/components/hhra.vue
+++ src/components/hhra.vue
@@ -1,194 +1,121 @@
 <template lang="pug">
   div
+    tov(@update='update')
+    h2 Accidental Soil Ingestion Dose
+    div `CDIsi = (Csoil * EF * ED * IRs * RBA * 0.000001) / (AT * BW) = `
+    input.result(v-model='CDIsi', ref='CDIsi', disabled)
     table
-      thead
-        tr
-          th Residential
-          th  
-          th Workers
-          th  
-          th Recreator
-          th  
-      tbody
-        tr
-          td Child
-          td Adult
-          td Outdoor
-          td Construction
-          td Child
-          td Adult
-        tr
-          td 350
-          td 350
-          td 225
-          td 250
-          td 75
-          td 75
-        tr
-          td 6
-          td 26
-          td 25
-          td 1
-          td 6
-          td 26
-        tr
-          td 1
-          td 1
-          td 0.333333333333333
-          td 0.333333333333333
-          td 0.041666666666667
-          td 0.041666666666667
-        tr
-          td 2190
-          td 9490
-          td 9125
-          td 350
-          td 2190
-          td 9490
-        tr
-          td 15
-          td 80
-          td 80
-          td 80
-          td 15
-          td 80
-        tr
-          td 200
-          td 100
-          td 100
-          td 330
-          td 200
-          td 100
-        tr
-          td 2337
-          td 6032
-          td 3527
-          td 3527
-          td 2337
-          td 6032
-        tr
-          td 0.2
-          td 0.07
-          td 0.12
-          td 0.3
-          td 0.2
-          td 0.07
+      hqparam(name='Csoil', units='mg/kg', v-model='Csoil', :desc='"concentration in soil (dry weight)"')
+      hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
+      hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
+      hqparam(name='IRs', units='mg/d', v-model='IRs', :desc='"ingestion rate soil"')
+      hqparam(name='RBA', units='unitless', v-model='RBA', :desc='"relative bioavailability factor for soil"')
+      hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
+      hqparam(name='BW', units='kg', v-model='BW', :desc='"body weight"')
 
-        h2 Accidental Soil Ingestion Dose
-        div `CDIsi = (Csoil * EF * ED * IRs * RBA * 0.000001) / (AT * BW) = `
-        input.result(v-model='CDIsi', ref='CDIsi', disabled)
-        table
-          hqparam(name='Csoil', units='mg/kg', v-model='Csoil', :desc='"concentration in soil (dry weight)"')
-          hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
-          hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
-          hqparam(name='IRs', units='mg/d', v-model='IRs', :desc='"ingestion rate soil"')
-          hqparam(name='RBA', units='unitless', v-model='RBA', :desc='""')
-          hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
-          hqparam(name='BW', units='kg', v-model='BW', :desc='"body weight"')
+    h2 Inhalation of Contaminated Particles Dose
+    div `CDIi nhal = (Csoil*EF*ED*ET*((1 / (VFs))+(1 / (PEFw)))/(AT)) = `
+    input.result(v-model='CDIinhal', ref='CDIinhal', disabled)
+    table
+      hqparam(name='Csoil', units='mg/kg', v-model='Csoil', :desc='"concentration in soil (dry weight)"')
+      hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
+      hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
+      hqparam(name='ET', units='mg/d', v-model='ET', :desc='"exposure time"')
+      hqparam(name='VFs', units='m3/kg', v-model='VFs', :desc='"volatilization factor"')
+      hqparam(name='PEFw', units='m3/kg', v-model='PEFw', :desc='"particulate emission factor"')
+      hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
 
-        h2 Inhalation of Contaminated Particles Dose
-        div `CDIi nhal = (Csoil*EF*ED*ET*((1 / (VFs))+(1 / (PEFw)))/(AT)) = `
-        input.result(v-model='CDIinhal', ref='CDIinhal', disabled)
-        table
-          hqparam(name='Csoil', units='mg/kg', v-model='Csoil', :desc='""')
-          hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
-          hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
-          hqparam(name='ET', units='mg/d', v-model='ET', :desc='"exposure time"')
-          hqparam(name='VFs', units='m3/kg', v-model='VFs', :desc='""')
-          hqparam(name='PEFw', units='m3/kg', v-model='PEFw', :desc='""')
-          hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
+    h2 Dermal contact with contaminated soil Dose Calculation
+    div `CDIderm = (Csoil*EF*ED*SA*AF*ABSd*0.000001)/(AT*BW) = `
+    input.result(v-model='CDIderm', ref='CDIderm', disabled)
+    table
+      hqparam(name='Csoil', units='mg/kg', v-model='Csoil', :desc='"concentration in soil (dry weight)"')
+      hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
+      hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
+      hqparam(name='SA', units='cm2/day', v-model='SA', :desc='"surface area"')
+      hqparam(name='AF', units='mg/cm2', v-model='AF', :desc='"soil adherence factor"')
+      hqparam(name='ABSd', units='proportion', v-model='ABSd', :desc='"dermal absorption"')
+      hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
+      hqparam(name='BW', units='kg', v-model='BW', :desc='"body weight"')
 
-        h2 Dermal contact with contaminated soil Dose Calculation
-        div `CDIderm = (Csoil*EF*ED*SA*AF*ABSd*0.000001)/(AT*BW) = `
-        input.result(v-model='CDIderm', ref='CDIderm', disabled)
-        table
-          hqparam(name='Csoil', units='mg/kg', v-model='Csoil', :desc='""')
-          hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
-          hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
-          hqparam(name='SA', units='cm2/day', v-model='SA', :desc='"surface area"')
-          hqparam(name='AF', units='mg/cm2', v-model='AF', :desc='"soil adherence factor"')
-          hqparam(name='ABSd', units='proportion', v-model='ABSd', :desc='""')
-          hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
-          hqparam(name='BW', units='kg', v-model='BW', :desc='"body weight"')
+    h2 Water Ingestion Dose Calculation
+    div `CDIwater = (Cwater*0.001*EF*ED*IRw)/(AT*BW) = `
+    input.result(v-model='CDIwater', ref='CDIwater', disabled)
+    table
+      hqparam(name='Cwater', units='ug/L', v-model='Cwater', :desc='"water concentration (total)"')
+      hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
+      hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
+      hqparam(name='IRw', units='L/day', v-model='IRw', :desc='"intake rate of water"')
+      hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
+      hqparam(name='BW', units='kg', v-model='BW', :desc='"body weight"')
 
-        h2 Water Ingestion Dose Calculation
-        div `CDIwater = (Cwater*0.001*EF*ED*IRw)/(AT*BW) = `
-        input.result(v-model='CDIwater', ref='CDIwater', disabled)
-        table
-          hqparam(name='Cwater', units='ug/L', v-model='Cwater', :desc='""')
-          hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
-          hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
-          hqparam(name='IRw', units='L/day', v-model='IRw', :desc='"intake rate of water"')
-          hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
-          hqparam(name='BW', units='kg', v-model='BW', :desc='"body weight"')
+    h2 Food Ingestion Dose Calculations
+    h3 Fish
+    div `CDIfish=(Cfish*EF*ED*IRfish*0.000001*CFfish)/(AT*BW) = `
+    input.result(v-model='CDIfish', ref='CDIfish', disabled)
+    table
+      hqparam(name='Cfish', units='mg/kg fresh weight', v-model='Cfish', :desc='"fish tissue concentrations"')
+      hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
+      hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
+      hqparam(name='IRfish', units='mg/day', v-model='IRfish', :desc='"intake rate of fish"')
+      hqparam(name='CFfish', units='proportion', v-model='CFfish', :desc='"contaminated fraction of fish"')
+      hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
+      hqparam(name='BW', units='kg', v-model='BW', :desc='"body weight"')
 
-        h2 Food Ingestion Dose Calculations
-        h3 Fish
-        div `CDIfish=(Cfish*EF*ED*IRfish*0.000001*CFfish)/(AT*BW) = `
-        input.result(v-model='CDIfish', ref='CDIfish', disabled)
-        table
-          hqparam(name='Cfish', units='mg/kg fresh weight', v-model='Cfish', :desc='""')
-          hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
-          hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
-          hqparam(name='IRfish', units='mg/day', v-model='IRfish', :desc='"intake rate of fish"')
-          hqparam(name='CFfish', units='proportion', v-model='CFfish', :desc='""')
-          hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
-          hqparam(name='BW', units='kg', v-model='BW', :desc='"body weight"')
+    h3 Produce
+    div `CDIp rod=(Cp rod*EF*ED*IRP*0.000001*CFp rod)/(AT*BW) = `
+    input.result(v-model='CDIprod', ref='CDIprod', disabled)
+    table
+      hqparam(name='Cprod', units='mg/kg fresh weight', v-model='Cprod', :desc='""')
+      hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
+      hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
+      hqparam(name='IRprod', units='mg/day', v-model='IRprod', :desc='"intake rate of produce"')
+      hqparam(name='CFprod', units='proportion', v-model='CFprod', :desc='"contaminated fraction of produce"')
+      hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
+      hqparam(name='BW', units='kg', v-model='BW', :desc='"body weight"')
 
-        h3 Produce
-        div `CDIp rod=(Cp rod*EF*ED*IRP*0.000001*CFp rod)/(AT*BW) = `
-        input.result(v-model='CDIprod', ref='CDIprod', disabled)
-        table
-          hqparam(name='Cprod', units='mg/kg fresh weight', v-model='Cprod', :desc='""')
-          hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
-          hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
-          hqparam(name='IRprod', units='mg/day', v-model='IRprod', :desc='""')
-          hqparam(name='CFprod', units='proportion', v-model='CFprod', :desc='""')
-          hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
-          hqparam(name='BW', units='kg', v-model='BW', :desc='"body weight"')
+    h3 Beef
+    div `CDIbeef=(Cbeef*EF*ED*IRbeef*0.000001*CFbeef)/(AT*BW) = `
+    input.result(v-model='CDIbeef', ref='CDIbeef', disabled)
+    table
+      hqparam(name='Cbeef', units='mg/kg fresh weight', v-model='Cbeef', :desc='"concentration in beef"')
+      hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
+      hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
+      hqparam(name='IRbeef', units='mg/day', v-model='IRbeef', :desc='"intake rate of beef"')
+      hqparam(name='CFbeef', units='proportion', v-model='CFbeef', :desc='"contaminated fraction of beef"')
+      hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
+      hqparam(name='BW', units='kg', v-model='BW', :desc='"body weight"')
 
-        h3 Beef
-        div `CDIbeef=(Cbeef*EF*ED*IRbeef*0.000001*CFbeef)/(AT*BW) = `
-        input.result(v-model='CDIbeef', ref='CDIbeef', disabled)
-        table
-          hqparam(name='Cbeef', units='mg/kg fresh weight', v-model='Cbeef', :desc='""')
-          hqparam(name='EF', units='days/year', v-model='EF', :desc='"exposure frequency"')
-          hqparam(name='ED', units='years', v-model='ED', :desc='"exposure duration"')
-          hqparam(name='IRbeef', units='mg/day', v-model='IRbeef', :desc='""')
-          hqparam(name='CFbeef', units='proportion', v-model='CFbeef', :desc='""')
-          hqparam(name='AT', units='days', v-model='AT', :desc='"averaging time (365 days*ED)"')
-          hqparam(name='BW', units='kg', v-model='BW', :desc='"body weight"')
+    h3 Hazard Quotient/Index
+    table
+      hqparam(name='TVoral', units='mg/kg-day', v-model='TVoral', disabled, :desc='""')
+      hqparam(name='TVinhal', units='mg/m3', v-model='TVinhal', disabled, :desc='""')
+      hqparam(name='CDIsi', units='mg/kg-day', v-model='CDIsi', :desc='""')
+      hqparam(name='CDIinhal', units='mg/m3', v-model='CDIinhal', :desc='""')
+      hqparam(name='CDIderm', units='mg/kg-day', v-model='CDIderm', :desc='""')
+      hqparam(name='CDIwater', units='mg/kg-day', v-model='CDIwater', :desc='""')
+      hqparam(name='CDIfish', units='mg/kg-day', v-model='CDIfish', :desc='""')
+      hqparam(name='CDIprod', units='mg/kg-day', v-model='CDIprod', :desc='""')
+      hqparam(name='CDIbeef', units='mg/kg-day', v-model='CDIbeef', :desc='""')
 
-        h3 Hazard Quotient/Index
-        table
-          hqparam(name='TVoral', units='mg/kg-day', v-model='TVoral', disabled, :desc='""')
-          hqparam(name='TVinhal', units='mg/m3', v-model='TVinhal', disabled, :desc='""')
-          hqparam(name='CDIsi', units='mg/kg-day', v-model='CDIsi', :desc='""')
-          hqparam(name='CDIinhal', units='mg/m3', v-model='CDIinhal', :desc='""')
-          hqparam(name='CDIderm', units='mg/kg-day', v-model='CDIderm', :desc='""')
-          hqparam(name='CDIwater', units='mg/kg-day', v-model='CDIwater', :desc='""')
-          hqparam(name='CDIfish', units='mg/kg-day', v-model='CDIfish', :desc='""')
-          hqparam(name='CDIprod', units='mg/kg-day', v-model='CDIprod', :desc='""')
-          hqparam(name='CDIbeef', units='mg/kg-day', v-model='CDIbeef', :desc='""')
-
-        input.result(v-model='HQsi', ref='HQsi')
-        input.result(v-model='HQinhal', ref='HQinhal')
-        input.result(v-model='HQderm', ref='HQderm')
-        input.result(v-model='HQwater', ref='HQwater')
-        input.result(v-model='HQfish', ref='HQfish')
-        input.result(v-model='HQprod', ref='HQprod')
-        input.result(v-model='HQbeef', ref='HQbeef')
+    input.result(v-model='HQsi', ref='HQsi')
+    input.result(v-model='HQinhal', ref='HQinhal')
+    input.result(v-model='HQderm', ref='HQderm')
+    input.result(v-model='HQwater', ref='HQwater')
+    input.result(v-model='HQfish', ref='HQfish')
+    input.result(v-model='HQprod', ref='HQprod')
+    input.result(v-model='HQbeef', ref='HQbeef')
 </template>
 
 <script>
 import $ from 'jquery'
 import MathJax from 'MathJax'
 import hqparam from './hqparam'
+import tov from './tov'
 
 export default {
   data () {
     return {
-      show: true,
       Csoil: 1,
       Cwater: 1,
       Cfish: 1,
@@ -214,10 +141,14 @@ export default {
       SA: 1,
       AF: 1,
       TVoral: 1,
-      TVinhal: 1
+      TVinhal: 1,
+      value: ''
     }
   },
   methods: {
+    update (values) {
+      Object.assign(this, values)
+    },
     moveInput () {
       $('.result').each(function () {
         let offset = $(this).prev().offset()
@@ -231,7 +162,7 @@ export default {
     }
   },
   computed: {
-    CDIsi: function () {
+    CDIsi () {
       let v = (this.Csoil * this.EF * this.ED * this.IRs * this.RBA * 0.000001) / (this.AT * this.BW)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -239,7 +170,7 @@ export default {
         return ''
       }
     },
-    CDIinhal: function () {
+    CDIinhal () {
       let v = (this.Csoil * this.EF * this.ED * this.ET * ((1 / (this.VFs)) + (1 / (this.PEFw))) / (this.AT))
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -247,7 +178,7 @@ export default {
         return ''
       }
     },
-    CDIderm: function () {
+    CDIderm () {
       let v = (this.Csoil * this.EF * this.ED * this.SA * this.AF * this.ABSd * 0.00001) / (this.AT * this.BW)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -255,7 +186,7 @@ export default {
         return ''
       }
     },
-    CDIwater: function () {
+    CDIwater () {
       let v = (this.Cwater * 0.001 * this.EF * this.ED * this.IRw) / (this.AT * this.BW)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -263,7 +194,7 @@ export default {
         return ''
       }
     },
-    CDIfish: function () {
+    CDIfish () {
       let v = (this.Cfish * this.EF * this.ED * this.IRfish * 0.000001 * this.CFfish) / (this.AT * this.BW)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -271,7 +202,7 @@ export default {
         return ''
       }
     },
-    CDIprod: function () {
+    CDIprod () {
       let v = (this.Cprod * this.EF * this.ED * this.IRprod * 0.000001 * this.CFprod) / (this.AT * this.BW)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -279,7 +210,7 @@ export default {
         return ''
       }
     },
-    CDIbeef: function () {
+    CDIbeef () {
       let v = (this.Cbeef * this.EF * this.ED * this.IRbeef * 0.000001 * this.CFbeef) / (this.AT * this.BW)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -287,7 +218,7 @@ export default {
         return ''
       }
     },
-    HQsi: function () {
+    HQsi () {
       let v = (this.CDIsi / this.TVoral)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -295,7 +226,7 @@ export default {
         return ''
       }
     },
-    HQinhal: function () {
+    HQinhal () {
       let v = (this.CDIinhal / this.TVinhal)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -303,7 +234,7 @@ export default {
         return ''
       }
     },
-    HQderm: function () {
+    HQderm () {
       let v = (this.CDIderm / this.TVoral)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -311,7 +242,7 @@ export default {
         return ''
       }
     },
-    HQwater: function () {
+    HQwater () {
       let v = (this.CDIwater / this.TVoral)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -319,7 +250,7 @@ export default {
         return ''
       }
     },
-    HQfish: function () {
+    HQfish () {
       let v = (this.CDIfish / this.TVoral)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -327,7 +258,7 @@ export default {
         return ''
       }
     },
-    HQprod: function () {
+    HQprod () {
       let v = (this.CDIprod / this.TVoral)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -335,7 +266,7 @@ export default {
         return ''
       }
     },
-    HQbeef: function () {
+    HQbeef () {
       let v = (this.CDIbeef / this.TVoral)
       if (!isNaN(v) && isFinite(v)) {
         return v
@@ -344,7 +275,6 @@ export default {
       }
     }
   },
-
   mounted () {
     this.$nextTick(function () {
       MathJax.Hub.Queue(['Typeset', MathJax.Hub])
@@ -355,7 +285,8 @@ export default {
     })
   },
   components: {
-    hqparam: hqparam
+    hqparam: hqparam,
+    tov: tov
   }
 }
 </script>
---
+++ src/components/tov.vue
@@ -0,0 +1,62 @@
+
+<template lang="pug">
+  table
+    thead
+      tr
+        th Residential
+        th  
+        th Workers
+        th  
+        th Recreator
+        th  
+    tbody
+      tr
+        td Child
+        td Adult
+        td Outdoor
+        td Construction
+        td Child
+        td Adult
+      tr(v-for='row in Object.keys(data)')
+        td(v-for='v in data[row]' @click='update($event)') {{v}}
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        data: {
+          EF: [350, 350, 225, 250, 75, 75],
+          ED: [6, 26, 25, 1, 6, 26],
+          ET: [1, 1, 0.333333333333333, 0.333333333333333, 0.041666666666667, 0.041666666666667],
+          AT: [2190, 9490, 9125, 350, 2190, 9490],
+          BW: [15, 80, 80, 80, 15, 80],
+          IRs: [200, 100, 100, 330, 200, 100],
+          SA: [2337, 6032, 3527, 3527, 2337, 6032],
+          AF: [0.2, 0.07, 0.12, 0.3, 0.2, 0.07],
+          IRw: [0.78, 2.5, null, null, 0.12, 0.071]
+        }
+      }
+    },
+    methods: {
+      update (e) {
+        let values = {}
+        for (let key in this.data) {
+          values[key] = this.data[key][e.target.cellIndex]
+        }
+        console.log(values)
+        this.$emit('update', values)
+      }
+    },
+    mounted () {
+      this.$el.querySelectorAll('td').forEach(td => td.addEventListener('click', this.update))
+    }
+  }
+</script>
+
+<style lang="stylus">
+  .child
+    background red
+</stylus>
+  
